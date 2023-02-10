fb6fa3471836d732660389162f4d0315f8e45314
Soheil <soheiltajik@outlook.com>
2023-02-10 22:00:24 +0330

    Add Main.java

--- src/main/java/com/soheiltjk/Main.java
+++ src/main/java/com/soheiltjk/Main.java
@@ -1,7 +1,12 @@
 package com.soheiltjk;
 
+import java.awt.*;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, FontFormatException {
+
+        Requirements gameRequirements = new Requirements();
+        gameRequirements.mainFrame();
     }
 }
