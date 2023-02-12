3e96b43d8db31d56a0a4ec6606f9be7a864af7df
Soheil <soheiltajik@outlook.com>
2023-02-12 17:52:15 +0330

    Update Main.java

--- src/main/java/com/soheiltjk/Main.java
+++ src/main/java/com/soheiltjk/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, FontFormatException {
-
+        System.out.println("https://github.com/SoheilTjk/game-requirements");
         Requirements gameRequirements = new Requirements();
         gameRequirements.mainFrame();
     }
